960e3ff1a958914b75af3815f68f643835039659
hadan24 <ha.dan.t02@gmail.com>
2026-02-24 19:51:37 -0800

    Set up config, outlined basic flow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,44 @@
+use clap::Parser;
+
 fn main() {
+    let config = Config::parse();
+    dbg!(&config);
+
+    // given bed & wakeup times, find how many cycles can fit
+    if config.bedtime.is_some() && config.wakeup.is_some() {
+        todo!()
+    }
+    // given chosen wakeup time, calculate bedtimes
+    else if config.wakeup.is_some() {
+        todo!()
+    }
+    // given chosen bedtime, calculate wakeup times
+    else if config.bedtime.is_some() {
+        todo!()
+    }
+    // default behavior: given bedtime of now, calculate wakeup times
+    else {
+        initial_test();
+    }
+}
+
+#[derive(Parser, Debug)]
+struct Config {
+    #[arg(short, long)]
+    bedtime: Option<String>,
+
+    #[arg(short, long)]
+    wakeup: Option<String>,
+
+    #[arg(short, long, default_value_t = false)]
+    nap: bool,
+
+    // 12-hour mode (default) or 24-hour mode
+    #[arg(short, long, default_value_t = false)]
+    mode24: bool
+}
+
+fn initial_test() {
     let now = time::OffsetDateTime::now_local()
         .expect("Could not get timezone offset.")
         .truncate_to_second()
