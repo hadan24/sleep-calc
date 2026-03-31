3041941e7d28738b0dd0626c6d1af4101393494c
hadan24 <ha.dan.t02@gmail.com>
2026-03-31 13:21:19 -0700

    Found `time`'s formatting tools 💀 (old attempt moved to ignored file)

--- src/io.rs
+++ src/io.rs
@@ -1,77 +1,28 @@
-use time::Time;
+use time::{
+    Time,
+    macros::format_description
+};
 use cli_table::{format, Cell, Style, Table};
 
-/*
-no am/pm
-    split on ':', plug in h/m
-w/ am/pm
-    strip + keep
-    if am, plug in h/m
-    if pm, plug in (h+12)/m
-*/
-pub fn parse_time(s: String) -> Result<Time, Box<dyn std::error::Error>> {
-    let s = s.trim().to_lowercase();
-    let suffixes = ["am", "pm"];
-    let (s, found_suffix) = suffixes.iter()
-        // apply callable that returns Option on each iter element/
-        // return 1st non-None item, or None itself if all were None
-        .find_map(|&suffix| {
-            s.strip_suffix(suffix)
-                .map(|stripped| (stripped.to_string(), Some(suffix)))
-        })
-        .unwrap_or((s, None));  // unwrap find_map Option or set to default value
-    let mut it = s.split(":");
 
-    match found_suffix {
-        Some(suffix) => {
-            let h = if suffix == "am" {
-                let temp = it.next().unwrap().parse::<u8>()?;
-                if temp == 12 {
-                    temp - 12
-                } else {
-                    temp
-                }
-            } else {
-                (it.next().unwrap().parse::<u8>()? + 12) % 24
-            };
-            Ok(Time::from_hms(h, it.next().unwrap().parse()?, 0)?)
-        },
-        None => {   // no am/pm
-            let (h, m) = (
-                it.next().unwrap().parse::<u8>()?,
-                it.next().unwrap().parse::<u8>()?
-            );
-            Ok(Time::from_hms(h, m, 0)?)
-        }
-    }
+pub fn parse_time(s: String) -> Result<Time, Box<dyn std::error::Error>> {
+    // prep valid fmts
+    // check against each
+    // propagate if all are Err
+    // otherwise ret Time
+    todo!()
 }
 
-pub fn format_time(t: &Time, format_options: &crate::config::FormatOptions) -> String {
-    let (h, m) = (t.hour(), t.minute());
-
-    if format_options.mode24 {
-        match m {
-            ..10 => format!("{h}:0{m}"),
-            10.. => format!("{h}:{m}")
-        }
+pub fn format_time(t: &Time, format_options: &crate::config::FormatOptions) ->
+    String  //Result<String, Box<dyn std::error::Error>> 
+{
+    let fmt_desc = if format_options.mode24 {
+        format_description!("[hour padding:space]:[minute]")
     }
     else {
-        let mut ftime = String::new();
-        match h {
-            0       => ftime.push_str("12"),
-            1..=12  => ftime.push_str(&format!("{h}")),
-            13..    => ftime.push_str(&format!("{}", h-12)),
-        }
-        match m {
-            ..10 => ftime.push_str(&format!(":0{m}")),
-            10.. => ftime.push_str(&format!(":{m}"))
-        }
-        match h {
-            ..13 => ftime.push_str(" AM"),
-            13.. => ftime.push_str(" PM"),
-        }
-        ftime
-    }
+        format_description!("[hour padding:space repr:12]:[minute] [period case:upper]")
+    };
+    t.format(fmt_desc).unwrap()
 }
 
 pub fn build_table(rows: Vec<Vec<cli_table::CellStruct>>, times_col_title: &str)
@@ -85,5 +36,6 @@ pub fn build_table(rows: Vec<Vec<cli_table::CellStruct>>, times_col_title: &str)
         .title( vec!["# Cycles".cell().bold(true), times_col_title.cell().bold(true)] )
         .separator(sep.build())
         .display()
-        .expect("Could not format table.")
+        .inspect_err(|e| eprintln!("Could not format table: {}", e))
+        .unwrap()   // if can't format, abort since std::io::Error is typically OS issue
 }
